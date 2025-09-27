9a28457da358baf0cd521178a318e5d6dd2b4a3d
GuilhermeMViana <gmunizviana@gmail.com>
2025-09-27 05:07:15 -0300

    feat(api-transactions): adding contexto do filter chart by id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import CnpjIDProvider from "./contexts/cnpj-id";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-200`}
       >
-        {children}
+        <CnpjIDProvider>
+          {children}
+        </CnpjIDProvider>
       </body>
     </html>
   );
